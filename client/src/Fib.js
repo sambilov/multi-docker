a4fedc5796ea8867781e14752f9a2fb45b33c8e4
Ferrus Bismalov <ferrusbismalov@vlad-sam-mb.local>
2020-01-24 22:48:05 +0000

    initial commit

---
+++ client/src/Fib.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Fib extends Component {
+    state = {
+        seenIndexes: [],
+        values: {},
+        index: '',
+    };
+
+    componentDidMount() {
+        this.fetchValues();
+        this.fetchIndexes();
+    }
+
+    async fetchValues() {
+        const values = await axios.get('api/values/current');
+
+        this.setState({ values: values.data });
+    }
+
+    async fetchIndexes() {
+        const seenIndexes = await axios.get('api/values/all');
+
+        this.setState({ seenIndexes: seenIndexes.data });
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const { index } = this.state;
+
+        await axios.post('/api/values', { index });
+        this.setState({ index: '' });
+    };
+
+    render() {
+        const { seenIndexes, values, index } = this.state;
+
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Enter your index:</label>
+                    <input value={index} onChange={event => this.setState({ index: event.target.value })} />
+                    <button>Submit</button>
+                </form>
+                <h3>Indexes I have seen:</h3>
+                {seenIndexes.map(({ number }) => number).join(', ')}
+                <h3>Calculated Values:</h3>
+                {Object.keys(values).map(key => (
+                    <div key={key}>For index {key} I calculated {values[key]}</div>
+                ))}
+            </div>
+        );
+    }
+}
